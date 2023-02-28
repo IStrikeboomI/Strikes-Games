609ca98504cc22da787b7d6d986283f7409bb4cd
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-02-27 20:47:51 -0600

    Names won't be too long now

--- src/main/java/Strikeboom/StrikesGames/dto/LobbyDto.java
+++ src/main/java/Strikeboom/StrikesGames/dto/LobbyDto.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.List;
 
@@ -12,10 +16,15 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Validated
 public class LobbyDto {
+    @NotBlank(message = "name must not be empty!")
+    @Size(max = 40,message = "Name is too long! Max is 40 characters")
     private String name;
+    @NotBlank(message = "game name must not be empty!")
     private String game;
     private boolean isPrivate;
+    @Positive(message = "max players must be positive!")
     private int maxPlayers;
     private Instant created;
     private String joinCode;
--- src/main/java/Strikeboom/StrikesGames/entity/Lobby.java
+++ src/main/java/Strikeboom/StrikesGames/entity/Lobby.java
@@ -7,7 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.List;
 
@@ -21,12 +23,14 @@ public class Lobby {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
     @NotBlank(message = "name must not be empty!")
+    @Size(max = 40,message = "Name is too long! Max is 40 characters")
     private String name;
     @NotBlank(message = "game name must not be empty!")
     private String game;
     private boolean isPrivate;
     @Positive(message = "max players must be positive!")
     private int maxPlayers;
+    @PastOrPresent
     private Instant created;
     //separate from the uuid where this is used to make a quick join url by being only 7 characters long, used in the URL
     private String joinCode;
