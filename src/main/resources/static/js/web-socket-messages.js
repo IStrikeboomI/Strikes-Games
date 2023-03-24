3535a17516fc0f358c02e57f6134d94292e2ca1a
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-03-23 20:38:09 -0500

    Added capability to handle a user joining updating the user list

--- src/main/resources/static/js/web-socket-messages.js
+++ src/main/resources/static/js/web-socket-messages.js
@@ -5,17 +5,17 @@ const handlers = [
 function userJoined(message) {
     let div = document.createElement("div");
     div.className = "user";
-    div.setAttribute("separationId",user.separationId);
+    div.setAttribute("separationId",message.user.separationId);
 
     //used for showing the user name
     let username = document.createElement("span");
     username.className = "username"
-    username.innerHTML = user.name
+    username.innerHTML = message.user.name
 
     //used for extra attributes such as Lobby Creator or You!
     let userAttributes = document.createElement("span");
     userAttributes.className="userAttributes"
-    userAttributes.innerHTML = " "+ (user.creator ? " (Lobby Creator)" : "");
+    userAttributes.innerHTML = " "+ (message.user.creator ? " (Lobby Creator)" : "");
 
     div.appendChild(username);
     div.appendChild(userAttributes);
