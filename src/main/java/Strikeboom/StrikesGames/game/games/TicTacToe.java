f5ee0c4d667fbcb701d1109741441825a0a3bc57
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-06-09 22:01:27 -0500

    Tic tac toe boiler plate

--- src/main/java/Strikeboom/StrikesGames/game/games/TicTacToe.java
+++ src/main/java/Strikeboom/StrikesGames/game/games/TicTacToe.java
@@ -1,13 +1,21 @@
 package Strikeboom.StrikesGames.game.games;
 
 import Strikeboom.StrikesGames.entity.Lobby;
+import Strikeboom.StrikesGames.entity.User;
 import Strikeboom.StrikesGames.game.GameInfo;
 import Strikeboom.StrikesGames.game.Games;
 import Strikeboom.StrikesGames.game.TurnBasedGame;
 
+import java.util.Random;
+
 public class TicTacToe extends TurnBasedGame {
+    User playerWithX;
+    User playerWithO;
     public TicTacToe(Lobby lobby) {
         super(lobby);
+        int randomUser = new Random().nextInt(2);
+        playerWithX = lobby.getUsers().get(randomUser);
+        playerWithO = lobby.getUsers().get(lobby.getUsers().size() - randomUser);
     }
 
     @Override
--- src/main/resources/static/js/game/tic-tac-toe.js
+++ src/main/resources/static/js/game/tic-tac-toe.js
@@ -4,3 +4,20 @@ let user2 = lobby.users[1]
 let canvas = document.createElement("canvas");
 
 document.body.appendChild(canvas);
+
+let users = document.createElement("div");
+users.id = "users";
+
+let user1Element = document.createElement("p");
+user1Element.className = "user";
+user1Element.innerHTML = user1.name;
+user1Element.setAttribute("separationId",user.separationId);
+users.appendChild(user1Element);
+
+let user2Element = document.createElement("p");
+user2Element.className = "user";
+user2Element.innerHTML = user2.name;
+user2Element.setAttribute("separationId",user.separationId);
+users.appendChild(user2Element);
+
+document.body.appendChild(users);
