f1f6f412bdc23f9a6dfef893663db9baee562810
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-07-27 21:48:53 -0500

    Card deck slides onto screen

--- src/main/resources/static/js/game/seth-head.js
+++ src/main/resources/static/js/game/seth-head.js
@@ -1,4 +1,44 @@
 
 function init() {
+    let canvas = document.createElement("canvas");
+    canvas.height = document.documentElement.clientHeight;
+    if (document.documentElement.clientWidth <= 600) {
+        canvas.width = document.documentElement.clientWidth;
+    } else {
+        canvas.width = document.documentElement.clientWidth;
+    }
 
+    const ctx = canvas.getContext("2d");
+    const img = new Image(100,140);
+    img.src = "card/back.png"
+    img.onload = (e) => {
+        window.requestAnimationFrame(animate);
+    }
+
+    function animate(timestamp) {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        if ((timestamp) <= (canvas.width / 2 - img.width/2)) {
+            //Everything here in this block is to show the card deck spinning onto the screen
+            ctx.save();
+            let imageX = (timestamp);
+            ctx.translate(imageX,canvas.height/2-img.height/2);
+            let rotation = (timestamp / (canvas.width / 2 - img.width/2)) * 4 * Math.PI;
+            ctx.rotate(rotation);
+            ctx.translate(-imageX,-(canvas.height/2-img.height/2));
+            for (let i = 0; i < 40;i++) {
+                ctx.drawImage(img,imageX,canvas.height/2-img.height/2-i,img.width,img.height);
+            }
+            ctx.restore();
+        } else {
+            //this block is to deal out the cards
+            ctx.drawImage(img,0,0,img.width,img.height);
+            ctx.resetTransform();
+            for (let i = 0; i < 40;i++) {
+                ctx.drawImage(img,canvas.width / 2 - img.width/2,canvas.height/2-img.height/2-i,img.width,img.height);
+            }
+        }
+        window.requestAnimationFrame(animate);
+    }
+
+    document.body.appendChild(canvas);
 }
