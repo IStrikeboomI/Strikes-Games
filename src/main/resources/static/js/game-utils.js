5099f96d65452281ce738ffd3b9551ea4d738214
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-07-28 22:37:28 -0500

    added card handling for clients

---
+++ src/main/resources/static/js/game-utils.js
@@ -0,0 +1,53 @@
+const cardNames = ["S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJ","CQ","CK","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK","RJ","BJ"];
+const cards = [];
+
+const Suit = Object.freeze({
+    SPADES: "S",
+    CLUBS: "C",
+    HEARTS : "H",
+    DIAMONDS: "D",
+    //used for jokers
+    RED: "R",
+    BLACK: "B"
+});
+const Value = Object.freeze({
+    ONE: "1",
+    TWO: "2",
+    THREE: "3",
+    FOUR: "4",
+    FIVE: "5",
+    SIX: "6",
+    SEVEN: "7",
+    EIGHT: "8",
+    NINE: "9",
+    TEN: "10",
+    JACK: "J",
+    QUEEN: "Q",
+    KING: "K",
+    JOKER: "J"
+});
+for (let name of cardNames) {
+    let suit = name.substring(0,1);
+    let value = name.substring(1);
+	let image = new Image();
+	image.src = `/image/card/${name}.png`
+    let card = {
+        name: name,
+        card: Object.entries(Suit).find((s) => s[1]===suit)[0],
+		value: Object.entries(Value).find((s) => s[1]===value)[0],
+		image: image
+    }
+	cards.push(card);
+}
+let backImage = new Image();
+backImage.src = "/image/card/back.png"
+let back = {
+    name: "back",
+    card: null,
+    value: null,
+    image: backImage
+}
+cards.push(back);
+function getCard(name) {
+	return cards.find(c => c.name===name);
+}
--- src/main/resources/static/js/game/seth-head.js
+++ src/main/resources/static/js/game/seth-head.js
@@ -1,6 +1,7 @@
-
+let canvas;
+let ctx;
 function init() {
-    let canvas = document.createElement("canvas");
+    canvas = document.createElement("canvas");
     canvas.height = document.documentElement.clientHeight;
     if (document.documentElement.clientWidth <= 600) {
         canvas.width = document.documentElement.clientWidth;
@@ -8,37 +9,34 @@ function init() {
         canvas.width = document.documentElement.clientWidth;
     }
 
-    const ctx = canvas.getContext("2d");
-    const img = new Image(100,140);
-    img.src = "card/back.png"
-    img.onload = (e) => {
-        window.requestAnimationFrame(animate);
-    }
-
-    function animate(timestamp) {
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        if ((timestamp) <= (canvas.width / 2 - img.width/2)) {
-            //Everything here in this block is to show the card deck spinning onto the screen
-            ctx.save();
-            let imageX = (timestamp);
-            ctx.translate(imageX,canvas.height/2-img.height/2);
-            let rotation = (timestamp / (canvas.width / 2 - img.width/2)) * 4 * Math.PI;
-            ctx.rotate(rotation);
-            ctx.translate(-imageX,-(canvas.height/2-img.height/2));
-            for (let i = 0; i < 40;i++) {
-                ctx.drawImage(img,imageX,canvas.height/2-img.height/2-i,img.width,img.height);
-            }
-            ctx.restore();
-        } else {
-            //this block is to deal out the cards
-            ctx.drawImage(img,0,0,img.width,img.height);
-            ctx.resetTransform();
-            for (let i = 0; i < 40;i++) {
-                ctx.drawImage(img,canvas.width / 2 - img.width/2,canvas.height/2-img.height/2-i,img.width,img.height);
-            }
-        }
-        window.requestAnimationFrame(animate);
-    }
-
+    ctx = canvas.getContext("2d");
     document.body.appendChild(canvas);
+    window.requestAnimationFrame(animate);
+}
+function animate(timestamp) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    let backImage = getCard("back").image;
+    backImage.width = 100;
+    backImage.height = 140;
+    if ((timestamp) <= (canvas.width / 2 - backImage.width/2)) {
+        //Everything here in this block is to show the card deck spinning onto the screen
+        ctx.save();
+        let imageX = (timestamp);
+        ctx.translate(imageX,canvas.height/2-backImage.height/2);
+        let rotation = (timestamp / (canvas.width / 2 - backImage.width/2)) * 4 * Math.PI;
+        ctx.rotate(rotation);
+        ctx.translate(-imageX,-(canvas.height/2-backImage.height/2));
+        for (let i = 0; i < 40;i++) {
+            ctx.drawImage(backImage,imageX,canvas.height/2-backImage.height/2-i,backImage.width,backImage.height);
+        }
+        ctx.restore();
+    } else {
+        //this block is to deal out the cards
+        ctx.drawImage(backImage,0,0,backImage.width,backImage.height);
+        ctx.resetTransform();
+        for (let i = 0; i < 40;i++) {
+            ctx.drawImage(backImage,canvas.width / 2 - backImage.width/2,canvas.height/2-backImage.height/2-i,backImage.width,backImage.height);
+        }
+    }
+    window.requestAnimationFrame(animate);
 }
