dae64a2a525e9f97b73907b1b7bbb3268f0d2690
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-01-29 22:03:08 -0600

    fixed some blurry/ugly image issues but still working on making the images look like the img tag

--- src/main/resources/static/js/game/base.js
+++ src/main/resources/static/js/game/base.js
@@ -9,11 +9,11 @@ class Game {
 		this.canvas.addEventListener('mousemove', (e) => this.onCanvasHover(e));
 
 		this.canvas.height = document.documentElement.clientHeight;
-		if (document.documentElement.clientWidth <= 600) {
-			this.canvas.width = document.documentElement.clientWidth;
-		} else {
-			this.canvas.width = document.documentElement.clientWidth;
-		}
+		this.canvas.width = document.documentElement.clientWidth;
+
+		//this.canvas.style.height = this.canvas.height + "px";
+		//this.canvas.style.width = this.canvas.width + "px";
+		this.canvas.style.imageRendering = 'pixelated';
 
 		document.body.appendChild(this.canvas);
 	}
@@ -22,8 +22,17 @@ class Game {
 			this.animationTimestamp = siteTimestamp;
 		}
 		let timestamp = siteTimestamp - this.animationTimestamp || 0;
-		this.canvas.getContext("2d").clearRect(0,0,this.canvas.width,this.canvas.height);
+		let ctx = this.canvas.getContext("2d",{antialias:false});
+		ctx.mozImageSmoothingEnabled    = false;
+		ctx.oImageSmoothingEnabled      = false;
+		ctx.webkitImageSmoothingEnabled = false;
+		ctx.msImageSmoothingEnabled     = false;
+		ctx.imageSmoothingEnabled       = false;
+
+		ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+		ctx.translate(.5,.5);
 		this.animationManager.drawAll(timestamp, timestamp -  this.lastTimestamp);
+		ctx.translate(-.5,-.5);
 		this.lastTimestamp = timestamp;
 		window.requestAnimationFrame(this.drawCanvas.bind(this));
 	}
