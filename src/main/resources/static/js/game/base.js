97bc1da15c5c2b27db7dbbeb14ed7a9531a2c297
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2024-02-02 19:55:14 -0600

    added device pixel ratio scaling

--- src/main/resources/static/js/game/base.js
+++ src/main/resources/static/js/game/base.js
@@ -7,13 +7,13 @@ class Game {
 		this.animationTimestamp;
 		this.canvas.addEventListener('mousedown', (e) => this.onCanvasClick(e));
 		this.canvas.addEventListener('mousemove', (e) => this.onCanvasHover(e));
+		window.addEventListener('resize', (e) => this.onResize(e));
 
-		this.canvas.height = document.documentElement.clientHeight;
-		this.canvas.width = document.documentElement.clientWidth;
+		this.canvas.height = document.documentElement.clientHeight * window.devicePixelRatio;
+		this.canvas.width = document.documentElement.clientWidth * window.devicePixelRatio;
 
-		//this.canvas.style.height = this.canvas.height + "px";
-		//this.canvas.style.width = this.canvas.width + "px";
-		this.canvas.style.imageRendering = 'pixelated';
+		this.canvas.style.height = (this.canvas.height / window.devicePixelRatio) + "px";
+		this.canvas.style.width = (this.canvas.width / window.devicePixelRatio) + "px";
 
 		document.body.appendChild(this.canvas);
 	}
@@ -22,19 +22,25 @@ class Game {
 			this.animationTimestamp = siteTimestamp;
 		}
 		let timestamp = siteTimestamp - this.animationTimestamp || 0;
-		let ctx = this.canvas.getContext("2d",{antialias:false});
-		ctx.mozImageSmoothingEnabled    = false;
-		ctx.oImageSmoothingEnabled      = false;
-		ctx.webkitImageSmoothingEnabled = false;
-		ctx.msImageSmoothingEnabled     = false;
-		ctx.imageSmoothingEnabled       = false;
+		let ctx = this.canvas.getContext("2d");
 
 		ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-		ctx.translate(.5,.5);
+		ctx.save();
+		ctx.scale(window.devicePixelRatio,window.devicePixelRatio);
+		console.log(window.devicePixelRatio);
+		ctx.imageSmoothingEnabled = true;
+		ctx.imageSmoothingQuality = "high";
 		this.animationManager.drawAll(timestamp, timestamp -  this.lastTimestamp);
-		ctx.translate(-.5,-.5);
+		ctx.restore();
 		this.lastTimestamp = timestamp;
 		window.requestAnimationFrame(this.drawCanvas.bind(this));
+	}
+	onResize(e) {
+		this.canvas.height = document.documentElement.clientHeight * window.devicePixelRatio;
+		this.canvas.width = document.documentElement.clientWidth * window.devicePixelRatio;
+
+		this.canvas.style.height = (this.canvas.height / window.devicePixelRatio) + "px";
+		this.canvas.style.width = (this.canvas.width / window.devicePixelRatio) + "px";
 	}
 	onCanvasHover(e) {
 		let x = e.layerX;
